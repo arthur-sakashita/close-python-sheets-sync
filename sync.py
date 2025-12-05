aada8e8bf6874ca5c54e21b6ce44317dcc8e03d7
arthur-sakashita <asakashita@onstrider.com>
2025-12-05 16:55:44 -0300

    Update sync.py

--- sync.py
+++ sync.py
@@ -161,8 +161,9 @@ BLOOMFIRE_FILTER = {
         "type": "and"
     },
     "include_counts": True,
-    "results_limit": 199,
-    "_limit":199,
+    "results_limit": 0,
+    "_limit":200,
+    "cursor":"5",
     "sort": []
 }
 
